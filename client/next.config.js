460cc0a1ecc60eb869a38cb12a774660fd9cf9fb
ThanatosCoil <thanatosxaos@gmail.com>
2025-06-15 14:21:41 +0300

    Add another route for second deployment

--- client/next.config.js
+++ client/next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
           {
             source: "/api/:path*",
             destination:
-              "https://e-commerce-project-production-47e2.up.railway.app/api/:path*",
+              "https://daring-curiosity-production.up.railway.app/api/:path*",
           },
         ];
   },
